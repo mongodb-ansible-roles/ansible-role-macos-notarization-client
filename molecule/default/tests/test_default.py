d2e5e8a594e69f7c0290ec68a6796d5f841936a9
Zakhar Kleyman <zakhar.kleyman@mongodb.com>
2021-08-27 12:26:47 -0400

    feat(BUILD-13765): create a role for ansible-role-macos-notarization-client

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,11 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_curator(host):
+    assert host.file("/usr/local/bin/macnotary").exists
